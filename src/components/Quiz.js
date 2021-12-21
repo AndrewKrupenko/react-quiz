a04b275f69e8e165c8d0a9e1aaa1c4d2cace07d1
kan <andrewkrupenko338@gmail.com>
2021-12-21 20:16:22 +0200

    added quiz state

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,15 +1,38 @@
 import Question from "./Question"
+import { useReducer } from "react"
+
+const initialState = {
+  questions: [],
+  currentQuestionIndex: 0,
+}
+
+const reducer = (state, action) => {
+  if (action.type === 'NEXT_QUESTION') {
+    return {
+      ...state,
+      currentQuestionIndex: state.currentQuestionIndex + 1,
+    }
+  }
+  return state
+}
 
 const Quiz = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className="quiz">
       <div>
-        <div className="score">Quiz</div>
+        <div className="score">Question 1/10</div>
         <Question />
-        <button className="next-button">Next Question</button>
+        <button
+          className="next-button"
+          onClick={() => dispatch({ type: "NEXT_QUESTION" })}
+        >
+          Next Question
+        </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Quiz
